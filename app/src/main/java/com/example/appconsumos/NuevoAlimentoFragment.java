83a7d291d494d6edba998a51afacdce1108557b5
phenriquez <pedro_18_90@hotmail.com>
2019-08-25 16:40:05 -0500

    done: funciones para tomar foto
    done: funciones para tomar foto

--- app/src/main/java/com/example/appconsumos/NuevoAlimentoFragment.java
+++ app/src/main/java/com/example/appconsumos/NuevoAlimentoFragment.java
@@ -1,14 +1,22 @@
 package com.example.appconsumos;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 /**
@@ -62,11 +70,52 @@ public class NuevoAlimentoFragment extends Fragment {
         }
     }
 
+    Button btnFotoNuevo;
+    ImageView imageViewFoto;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View nuevoAlimentoView = inflater.inflate(R.layout.fragment_nuevo_alimento, container, false);
+
+        //crear variables y accion para tomar foto
+        imageViewFoto = (ImageView) nuevoAlimentoView.findViewById(R.id.imageViewFotoNuevoAlimento);
+        btnFotoNuevo = (Button) nuevoAlimentoView.findViewById(R.id.buttonFotoNuevoAlimento);
+        btnFotoNuevo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // do something
+                tomarFotoIntent();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nuevo_alimento, container, false);
+        return nuevoAlimentoView;
+    }
+
+    //metodo para tomar foto
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private void tomarFotoIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    //metodo para aceptar o rechazar foto
+    //@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageViewFoto.setImageBitmap(imageBitmap);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
